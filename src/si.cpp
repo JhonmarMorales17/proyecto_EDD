df0d7debfa47e780df0f28e435a120c12d480852
Jhonmar Morales <jhonmar.morales.jr@gmail.com>
2025-04-10 19:47:28 -0400

    feat: Funciones

--- src/si.cpp
+++ src/si.cpp
@@ -42,5 +42,21 @@ class FamilyTree {
 private:
     FamilyMember* root;
     FamilyMember* current_chief;
+ FamilyMember* createMember(int id, string name, string last_name, char gender, 
+                             int age, int id_father, bool is_dead, bool was_chief, bool is_chief) {
+        FamilyMember* newMember = new FamilyMember();
+        newMember->id = id;
+        newMember->name = name;
+        newMember->last_name = last_name;
+        newMember->gender = gender;
+        newMember->age = age;
+        newMember->id_father = id_father;
+        newMember->is_dead = is_dead;
+        newMember->was_chief = was_chief;
+        newMember->is_chief = is_chief;
+        return newMember;
+    }
+    
+
     
     
